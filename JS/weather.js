981fb4b548fe930c4bbcbc1d52f4ea9cf578b80b
yeonju-s <nineelephant@naver.com>
2022-02-20 21:58:32 +0900

    Update for CSS

--- JS/weather.js
+++ JS/weather.js
@@ -5,10 +5,10 @@ function onGeoSuccess(position){
     const lat = position.coords.latitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data =>{
-        const weather = document.querySelector("#weather span:first-child")
-        const city = document.querySelector("#weather span:last-child")
+        const city = document.querySelector("#weather span:first-child")
+        const weather = document.querySelector("#weather span:last-child")
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        weather.innerText = `${data.main.temp}˚C, ${data.weather[0].main}`;
     })
 }
 
