c1772a91f3609c5dac4a3b5a765f4838c77a6098
yeonju-s <nineelephant@naver.com>
2022-02-21 23:17:03 +0900

    Update background.js

--- JS/background.js
+++ JS/background.js
@@ -1,5 +1,5 @@
 
-const imgs = ['imgs/10.jpg', 'imgs/2.jpg', 'imgs/3.jpg', 'imgs/4.jpg', 'imgs/5.jpg', 'imgs/6.jpg', 'imgs/7.jpg', 'imgs/8.jpg', 'imgs/9.jpg', 'imgs/11.jpg', 'imgs/12.jpg', 'imgs/13.jpg'];
+const imgs = ['imgs/2.jpg', 'imgs/3.jpg', 'imgs/5.jpg', 'imgs/6.jpg', 'imgs/8.jpg', 'imgs/9.jpg', 'imgs/10.jpg', 'imgs/11.jpg', 'imgs/12.jpg', 'imgs/13.jpg'];
 const body = document.body;
 const greeting = document.querySelector('.greeting');
 const img = document.createElement('img');
